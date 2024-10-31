5ffceb4ea4776d3ba325e5e94205ed056f112b05
Lehmazzilli <Hellenamazzilli8@gmail.com>
2024-10-31 17:51:04 -0300

    Add files via upload

---
+++ FlashCard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimento gerais',
+    'Oque é Yakisoba?',
+    'Uma comida tradicional japonesa que inclui macarrão com legumes e carne.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'Oque é Back-end?',
+    'É oque conecta o Front-end com o banco de dados e traz os mesmo, cria ou deleta'
+)
